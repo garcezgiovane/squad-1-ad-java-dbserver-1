60a08671f044f5e8dbff0a101155b7d43fdb7848
garcezgiovane <giovanegarcez16@gmail.com>
2019-11-10 11:57:27 -0300

    first commit

---
+++ src/main/java/com/aceleradev/codenation/CodenationApplication.java
@@ -0,0 +1,13 @@
+package com.aceleradev.codenation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodenationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aceleradev/codenation/CodenationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceleradev.codenation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodenationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
